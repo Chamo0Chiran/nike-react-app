555295036006c8b3e2ff36326d8751b3343929a2
Chamodh <chamochian05@gmail.com>
2024-09-30 21:36:55 +0530

    add: initial files

---
+++ src/componets/Button.jsx
@@ -0,0 +1,27 @@
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  borderColor,
+  textColor,
+  fullWidth,
+}) => {
+  return (
+    <button
+      className={`button-shop-now ${backgroundColor} ? ${backgroundColor} ${textColor} ${borderColor} ${
+        fullWidth && "w-full"
+      }`}
+    >
+      {label}{" "}
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+
+import Footer from "./Footer";
+import Services from "./Services";
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import PopularProducts from "./PopularProducts";
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Footer,
+  Services,
+  CustomerReviews,
+  SpecialOffers,
+  Subscribe,
+};
